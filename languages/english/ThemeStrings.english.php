9bd2d10730f9b2ffbde2199f66701b5cf6330d03
Burke Knight <github@burkeknight.com>
2015-07-04 21:08:57 -0400

    Initial Upload

---
+++ languages/english/ThemeStrings.english.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @name      ElkArte Forum
+ * @copyright ElkArte Forum contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
+ * This software is a derived product, based on:
+ *
+ * Simple Machines Forum (SMF)
+ * copyright:	2011 Simple Machines (http://www.simplemachines.org)
+ * license:  	BSD, See included LICENSE.TXT for terms and conditions.
+ *
+ * @version 1.0
+ *
+ */
+
+$txt['header_logo_url_desc'] = '(Leave blank to show forum name.)';
+$txt['site_slogan_desc'] = '(Add your own text for a slogan here. It will appear under the logo or title.)';
+
+$txt['custom_copyright'] = 'Custom Copyright';
+$txt['custom_copyright_desc'] = 'Your own copyright message that will appear above the default copyright. (HTML allowed)';
+
+$txt['custom_favicon'] = 'Custom Favicon';
+$txt['custom_favicon_desc'] = 'The full URL to a 196x196 Image to be used as the favicon for your site.';
+
+$txt['news2'] = 'Special News & Announcements';
+
+$txt['facebook'] = 'Facebook';
+$txt['flickr'] = 'Flickr';
+$txt['googleplus'] = 'Google+';
+$txt['instagram'] = 'Instagram';
+$txt['pinterest'] = 'Pinterest';
+$txt['stumbleupon'] = 'Stumbleupon';
+$txt['tumblr'] = 'Tumblr';
+$txt['twitter'] = 'Twitter';
+$txt['youtube'] = 'Youtube';
+$txt['rss'] = 'RSS';
+$txt['github'] = 'Github';
+$txt['steam'] = 'Steam';
+$txt['linkedin'] = 'Linkedin';
+
+$txt['facebook_id'] = 'Facebook';
+$txt['flickr_id'] = 'Flickr';
+$txt['googleplus_id'] = 'Google+';
+$txt['instagram_id'] = 'Instagram';
+$txt['pinterest_id'] = 'Pinterest';
+$txt['stumbleupon_id'] = 'Stumbleupon';
+$txt['tumblr_id'] = 'Tumblr';
+$txt['twitter_id'] = 'Twitter';
+$txt['youtube_id'] = 'Youtube';
+$txt['rss_id'] = 'Show RSS';
+$txt['github_id'] = 'Github';
+$txt['steam_id'] = 'Steam';
+$txt['linkedin_id'] = 'Linkedin';
+
+$txt['url_desc'] = 'Please enter complete url. Example, http://www.social.net/you/';
+
+
+?>
